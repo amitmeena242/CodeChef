3384efe4e98f1e681a0aab04cee6778af90185c8
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-09-18 11:40:58 +0530

    Create Hack It.py

---
+++ Hack It.py
@@ -0,0 +1,11 @@
+val=0
+for i in range(int(input())):
+    n=int(input())
+    n=str(n)
+    s=""
+    for j in n:
+        valn=int(j)
+        val=int(val)
+        val=valn-2
+        s=s+str(val)
+    print(s)

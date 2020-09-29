69d4c8fe33b766ad7c6821174ed0777b7b20ba69
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-09-29 17:02:45 +0530

    Create Loki The Destroyer.py

---
+++ Loki The Destroyer.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+
+def dfs(d,j,visited):
+    global dcount
+    
+    visited[j-1]=True
+    for i in d[j]:
+        if visited[i-1]==False:
+            dcount[cost[i-1]]+=1
+            dfs(d,i,visited)
+
+def prod(arr):
+    mod = 1000000007
+    res=1
+    for i in arr:
+        res*=i
+        res%=mod
+    return res
+
+n=int(input())
+cost=list(map(int,input().split()))
+d=defaultdict(list)
+for _ in range(int(input())):
+    a,b=map(int,input().split())
+    d[a].append(b)
+    d[b].append(a)
+arr=[]
+visited=[False]*n
+
+for i in d:
+    global dcount
+    dcount = defaultdict(int)
+    for j in d[i]:
+        if visited[j-1]==False:
+            dcount[cost[j-1]]+=1
+            dfs(d,j,visited)
+    if len(dcount):
+        minm = float("inf")
+        for i in dcount:
+            minm =min(i,minm)
+        arr.append(dcount[minm])
+print(prod(arr))
